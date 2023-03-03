f3d0b9a1954d51cbe5fa80aa8cdfdf5ba2051f03
Paulinha Figueiredo <116231419+PaulinhaFigueiredo@users.noreply.github.com>
2023-03-03 07:47:24 -0300

    Exercicio hq (#2)
    * Exercicios aula 1
    
    * Adicionando gitignore
    
    * Refatora arquivos
    
    * Subindo exercicio HQ da apostila

---
+++ primeira-aula/src/main/java/me/paula/exerciciosDeCasa/HQ.java
@@ -0,0 +1,36 @@
+package me.paula.exerciciosDeCasa;
+
+import java.math.BigDecimal;
+public class HQ {
+    private String nome;
+    private BigDecimal preco;
+    private String roteirista;
+    private String artista;
+    private String ISBN;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public void setRoteirista(String roteirista) {
+        this.roteirista = roteirista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    @Override
+    public String toString() {
+        return "HQ: \nnome=" + nome + "\npreco=" + preco + "\nroteirista=" + roteirista + "\nartista=" + artista
+                + "\nISBN=" + ISBN + "";
+    }
+}
